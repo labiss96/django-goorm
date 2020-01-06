738aef2e02b02f530545b175c31fa64520bcfb99
leejunghyun <cxz9080@gmail.com>
2020-01-06 22:26:08 +0900

    profile image 추가.(default 이미지) + static/media 설정.

--- Goorm_Project/urls.py
+++ Goorm_Project/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 import main.views
 import accounts.views
 import goorm.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('accounts/', include('accounts.urls')),
     path('goorm/', include('goorm.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup/',views.signup,name='signup'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
+    path('mypage/<str:profile_name>', views.mypage, name='mypage'),
 ]
