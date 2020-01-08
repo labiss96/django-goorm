b582ce350453dc30a8fe0689bca85644a7092a24
ParkDoEun <pde140300@gmail.com>
2020-01-08 14:47:49 +0900

    image

--- Goorm_Project/urls.py
+++ Goorm_Project/urls.py
@@ -4,9 +4,13 @@ import main.views
 import accounts.views
 import goorm.views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('accounts/', include('accounts.urls')),
     path('goorm/', include('goorm.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- goorm/models.py
+++ goorm/models.py
@@ -2,7 +2,7 @@ from django.db import models
 class Tobacco(models.Model):
     brand = models.CharField(max_length=100)
     name = models.CharField(max_length=50)
-    price = models.CharField(max_length=10)
+    price = models.IntegerField(blank=True)
     rel_date = models.DateTimeField(blank=True)
     nicotine =  models.FloatField()
     TAR = models.FloatField()
@@ -11,6 +11,11 @@ class Tobacco(models.Model):
     #total_like = models.PositiveIntegerField(default=0)
     #like_user = models.ManyToManyField()
     isMenthol = models.BooleanField(default = False)
+    img = models.ImageField(blank=True, null=True)
+    
 
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
