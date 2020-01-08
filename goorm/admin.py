aaa5aa114bd891ea632c9d829562103751a892de
(소프트웨어학부)이정현 <labiss96@kookmin.kr>
2020-01-08 16:30:20 +0900

    Brand 모델 생성 및 Tobacco모델과 일대다관계 설정.

--- goorm/admin.py
+++ goorm/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Tobacco
+from .models import Tobacco, Brand
 
 
 admin.site.register(Tobacco)
-
-
+admin.site.register(Brand)
 
 # Register your models here.
--- goorm/models.py
+++ goorm/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+
+class Brand(models.Model):
+    brd_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.brd_name
+
 class Tobacco(models.Model):
-    brand = models.CharField(max_length=100)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='tobacco')
     name = models.CharField(max_length=50)
     price = models.IntegerField(blank=True)
     rel_date = models.DateTimeField(blank=True)
